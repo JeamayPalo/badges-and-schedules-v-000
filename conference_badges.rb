65bcad85e1c6ab4ec89beb1693a883551e6fe236
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-10 20:00:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-    batch_badge_creator(attendees)
+    batch_badge_creator(attendees).each |badge|
     assign_rooms(attendees)
   end
 end
