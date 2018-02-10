813704776c85dcbd02df6f8acc312cd10dd162bb
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-10 19:36:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,8 +15,7 @@ def assign_rooms(attendees)
   empty_array = []
 	attendees.each_with_index do |attendee, index|
   empty_array <<  "Hello, #{attendee}! You'll be assigned to #{index}"
-      index = 1
-      index += 1
+      index + 1
   end
   empty_array
 end
