92601c33d5d0ff7763c138b9e2e28a659213e686
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-10 18:59:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,11 @@ end
 def batch_badge_creator(attendees)
   empty_array = []
   attendees.each do |badges|
-    empty_array << "Hello, my name is #{badges}."
+    empty_array << badge_maker(badges)
   end
   empty_array
 end
 
 def assign_rooms
+  
+end
